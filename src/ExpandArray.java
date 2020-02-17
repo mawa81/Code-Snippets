054275c3a9cdf09b20a68b7bd2a96503ead88c74
Mario Wagner <ma_wa@gmx.at>
2020-02-17 10:13:39 +0100

    Created new Snippet

---
+++ src/ExpandArray.java
@@ -0,0 +1,14 @@
+public class ExpandArray {
+    int[] tmp = new int[list.length + 1]; //
+            for(
+    int i = 0;
+    i<list.length;i++)
+
+    {
+        tmp[i] = list[i];
+    }
+
+    tmp[count]=number;
+    list =tmp;
+    count++;
+}
